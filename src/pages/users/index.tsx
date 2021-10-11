b7008a717594415ab0dbcecb797f79bb6923849f
Davi <davi.freitas@useall.com.br>
2021-10-11 11:20:09 -0300

    pagination component not static anymore

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,40 +17,16 @@ import {
 } from "@chakra-ui/react";
 import { useEffect } from "react";
 import Link from "next/link";
-import { useQuery } from "react-query";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
-    "users",
-    async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    }
-  );
+  const { data, isLoading, isFetching ,error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -69,7 +45,8 @@ export default function UserList() {
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
-              Listagem de usuários
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
             </Heading>
             <Link href="/users/create" passHref>
               <Button
@@ -138,7 +115,7 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination totalCountOfRegisters={200} currentPage={5} onPageChange={() => {}} />
             </>
           )}
         </Box>
